61febb324319bfa7a49fce81be3c821528cf6f89
ToBletkiOtUma <142865173+ToBletkiOtUma@users.noreply.github.com>
2025-04-05 16:38:01 +0500

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,6 +1,8 @@
 import { Color } from 'pixel_combats/basic';
 import { Teams } from 'pixel_combats/room';
 try{
-  Teams.Add('Player','Синие', new Color(0,0,1,0));
+  Teams.Add('Player','<b>Синие</b>', new Color(0,0,1,0));
+  Teams.Add('Player2','<b>Красные</b>', new Color(1,0,0,0));
+  
 }catch(e){
 }
